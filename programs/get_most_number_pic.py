4896e2f3ba4a126801e8606998f5e6683c5326fc
Qiming Liu <99009952+qimingliu1021@users.noreply.github.com>
2024-06-28 00:30:55 -0400

    Create get_most_number_pic.py
    Can now get the most number of pictures of one manufacture. 
    
    Haven't added the code to store the image

---
+++ programs/get_most_number_pic.py
@@ -0,0 +1,48 @@
+from selenium import webdriver
+from bs4 import BeautifulSoup
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import os
+import time
+
+url = "https://jinkecn.en.alibaba.com/productlist-1.html?filter=all&sortType=modified-desc&spm=a2700.shop_pl.41413.dbtmnavgo"
+html_directory = 'htmls_single_extract'
+html_logs = 'logs_single_extract'
+page_count = 0
+driver = webdriver.Edge()
+driver.get(url)
+wait = WebDriverWait(driver, 10)
+next_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "next-btn.next-btn-normal.next-btn-medium.next-pagination-item.next")))
+
+stopping_point = [
+  "next-row.next-row-no-padding.next-row-justify-space-between.next-row-align-center.nav-content",
+  "next-icon.next-icon-arrow-down.next-icon-medium", 
+  "next-pagination-jump", 
+  "copyright"
+]
+
+while next_button.is_enabled(): 
+  page_count += 1
+  print(f"at page {page_count}......")
+  # next-pagination-jump
+  for point_class in stopping_point: 
+    element = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, point_class)))
+    ActionChains(driver).move_to_element(element).perform()
+    time.sleep(2)
+  next_button.click()
+  find_image = False
+  # 
+  soup = BeautifulSoup(driver.page_source, 'html.parser')
+  if soup.find('div', class_="component-product-list"):
+    component_product_list = soup.find('div', class_="component-product-list")
+    if component_product_list.find_all('img', class_='react-dove-image'): 
+      img_list = component_product_list.find_all('img', class_='react-dove-image')
+      print(f"find {len(img_list)} images \n")
+  next_button = wait.until(EC.element_to_be_clickable((By.CLASS_NAME, "next-btn.next-btn-normal.next-btn-medium.next-pagination-item.next")))
+
+with open("single_extract.html", "w") as file: 
+  file.write(soup.prettify())
+
+driver.quit()
